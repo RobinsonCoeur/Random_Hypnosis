4f6f2e045e4685455cd02bb3d80c14224c8a5afa
lolodel <baguette.production.co@gmail.com>
2023-12-30 21:53:16 +0100

    videos load in fullscreen, delay between videos at 0 scale fix

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import keyboard
 import random
 
-import webbrowser
-
 import vlc
+
+import webbrowser
 
 import sched
 import time
@@ -41,9 +41,17 @@ class GUI:
         return entry
     
     def checkFrameConditions(self):
-        
-        self.scaleValue.configure(text = str(self.scale1.get()) + " hours")
+        val = str(self.scale1.get()).split(".")
+        hrs = val[0]
+        inMin = int(val[1])
+
+        if inMin == 5:
+            inMin = 50
+
+        self.scaleValue.configure(text = hrs + " hr and " + str(inMin*60/100).split(".")[0] + " min")
         self.root.after(50, self.checkFrameConditions)
+
+        self.launchTimeRange = int(self.scale1.get()*60*60) + 1
 
         return
 
@@ -62,19 +70,19 @@ class GUI:
                                   command= lambda : self.loadVlcDownloadPage())
         menuFrame.buttonVLC.place(relx = 0.5, rely = 0.6, anchor = CENTER)
 
-        label = CTkLabel(menuFrame, width = 100, text = "Input max time without video", fg_color = ("white", "#1c6ba3"), corner_radius=8)
+        label = CTkLabel(menuFrame, width = 100, text = "Input max time between videos", fg_color = ("white", "#1c6ba3"), corner_radius=8)
         label.pack(side = BOTTOM)
 
         #time input
         bottomFrame = CTkFrame(menuFrame, height = 40, width = 350, fg_color = "#2b2b2b", corner_radius=5)
         bottomFrame.pack(side = BOTTOM)
 
-        self.scale1 = CTkSlider(bottomFrame, from_= 0, to=10, orientation="horizontal", number_of_steps = 20)
-        self.scale1.place(relx = 0.47, rely = 0.5, anchor = CENTER)
+        self.scale1 = CTkSlider(bottomFrame, from_= 0, to=5, orientation="horizontal", number_of_steps = 20)
+        self.scale1.place(relx = 0.3, rely = 0.5, anchor = CENTER)
 
         self.scaleValue = CTkLabel(bottomFrame, width = 10, text = "0", fg_color = ("white", "#1c6ba3"), corner_radius= 5)
-        self.scaleValue.place(relx = 0.90, rely = 0.5, anchor = CENTER)
-        
+        self.scaleValue.place(relx = 0.80, rely = 0.5, anchor = CENTER)
+
         return
 
     def launch(self, linkEntry):
@@ -83,13 +91,20 @@ class GUI:
         self.randomEvent(filepath)
 
     def video(self, file):
-        self.mediaPlayer.toggle_fullscreen()
+        self.mediaPlayer.set_fullscreen(True)
         media = vlc.Media(file)
         self.mediaPlayer.set_media(media)
  
         def videoThread():
+            start = time.time()
+
             self.mediaPlayer.play()
             time.sleep(1)
+
+            while True:
+                if time.time() - start >= self.mediaPlayer.get_length()/1000 - 0.5:
+                    self.mediaPlayer.stop()
+                    break
 
         t1=Thread(target=videoThread)
         t1.start()
@@ -106,20 +121,37 @@ class GUI:
     def randomEvent(self, folderPath):
         eventSchedule = sched.scheduler(time.time, time.sleep)
 
+        def arrangeBoundaries(val1, val2):
+            if val1 > val2:
+                b = val1
+                a = val2
+            else:
+                a = val1
+                b = val2
+            return a, b
+
         def loadVideo():
             files = os.listdir(folderPath)
             chosenFile = files[random.randint(0, len(files)-1)]
+            chosenFile = "boobs (online-video-cutter.com).mp4"
 
             self.video(r"{}".format(folderPath + "\\" + chosenFile))
             time.sleep(1)
 
-            delay = self.mediaPlayer.get_length()/1000 - 1
+            delay = int(self.mediaPlayer.get_length()/1000) + 5 #make sure the mediaplayer has time to stop and start again
 
-            eventSchedule.enter(random.randint(delay, self.launchTimeRange), 1, loadVideo)
+            if self.launchTimeRange > delay:
+                a, b = delay, self.launchTimeRange
+            else:
+                a, b = delay, delay + 2
 
-        eventSchedule.enter(30, 1, loadVideo)
+            eventSchedule.enter(random.randint(a, b), 1, loadVideo)
+
+        a, b = arrangeBoundaries(5, self.launchTimeRange)
+        eventSchedule.enter(random.randint(a, b), 1, loadVideo)
         eventSchedule.run()    
     
+
     def clearFrame(self, frame: CTkFrame):
         for widget in frame.winfo_children():
             widget.destroy()
